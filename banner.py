67d525cce8eb94aca163b3d98562f37716f9809c
iaacornus <iaacornus.devel@gmail.com>
2022-08-05 01:34:29 +0800

    use " for consistency

--- banner.py
+++ banner.py
@@ -13,17 +13,17 @@ class CopyPasta():
         self.terminal_size = get_terminal_size(fallback=(120, 50))
 
     def get(self) -> str:
-        '''
+        """
         Gets a random copypasta from r/copypasta
 
         :returns str: The copypasta text
-        '''
+        """
 
         for _ in range(10): # max 10 tries
-            subreddit = 'copypasta'
+            subreddit = "copypasta"
             count = 1
-            timeframe = 'all'
-            listing = 'random'
+            timeframe = "all"
+            listing = "random"
 
             headers = {
                     "Upgrade-Insecure-Requests": "1",
@@ -48,7 +48,7 @@ class CopyPasta():
                 }
 
             def get_reddit(subreddit, count) -> dict | None:
-                '''
+                """
                 get_reddit(subreddit, amount) -> dict or None
 
                 Gets the posts from the specified subreddit
@@ -57,12 +57,12 @@ class CopyPasta():
                 :param count str: Amount of posts to load
                 :returns None or dict: Json dictionary of comments
                 if no errors ocurred, else None
-                '''
+                """
 
                 try:
                     base_url = (
-                            f'https://www.reddit.com/r/{subreddit}'
-                            f'/{listing}.json?count={count}&t={timeframe}'
+                            f"https://www.reddit.com/r/{subreddit}"
+                            f"/{listing}.json?count={count}&t={timeframe}"
                         )
                     request = get(
                             base_url, headers=headers, timeout=10
@@ -77,24 +77,24 @@ class CopyPasta():
                 sleep(2) # sleep 2 seconds first
                 continue
 
-            if listing != 'random':
-                title = top_post['data']['children'][0]['data']['title']
-                url = top_post['data']['children'][0]['data']['url']
+            if listing != "random":
+                title = top_post["data"]["children"][0]["data"]["title"]
+                url = top_post["data"]["children"][0]["data"]["url"]
             else:
-                title = top_post[0]['data']['children'][0]['data']['title']
-                url = top_post[0]['data']['children'][0]['data']['url']
+                title = top_post[0]["data"]["children"][0]["data"]["title"]
+                url = top_post[0]["data"]["children"][0]["data"]["url"]
 
             def get_post() -> dict | None:
-                '''
+                """
                 get_post() -> dict or None
 
                 Gets the posts info
 
                 :returns dict or None: Json dictionary if no errors ocurred, else None
-                '''
+                """
 
                 try:
-                    request = get(f'{url}.json', headers=headers, timeout=10)
+                    request = get(f"{url}.json", headers=headers, timeout=10)
                     return request.json()
                 except Exception:
                     return None
@@ -104,40 +104,40 @@ class CopyPasta():
                 if not selftext:
                     continue
 
-                selftext = selftext[0]['data']['children'][0]['data']['selftext']
+                selftext = selftext[0]["data"]["children"][0]["data"]["selftext"]
                 text2 = unquote(selftext.strip().replace(r"&amp;#x200B;","").replace("\n"," ").replace("      ",""))
 
-                if text2 != "" and not 'removed by reddit' in text2.lower():
+                if text2 != "" and not "removed by reddit" in text2.lower():
                     return text2
 
             except Exception:
                 continue
 
-        return 'Failed to get copypasta :('
+        return "Failed to get copypasta :("
 
 
 def gethalf(x) -> int:
-    '''
+    """
     gethalf(text) -> int
 
     Gets half of the lines of the text specified
 
     :param x str: Text to calculate
     :returns int: Amount of lines
-    '''
+    """
 
-    return round(len(x.split('\n')/2)) # gets the length of the specified string, splits it and then counts the amount of items
+    return round(len(x.split("\n")/2)) # gets the length of the specified string, splits it and then counts the amount of items
 
 
 def makeblock(x) -> str:
-    '''
+    """
     makeblock(ascii art) -> list
 
     Turns the ASCII art in a perfect block, to prevent lines from shifting
 
     :param x str: The ASCII art to block-ify
     :returns str: Block-ified ASCII art
-    '''
+    """
 
     ascii_art1 = []
     ascii_art11 = []
@@ -149,13 +149,13 @@ def makeblock(x) -> str:
         if x != "":
             ascii_art11.append(f"{x+(' '*(size-len(x)))}")
 
-    return '\n'.join(ascii_art11)
+    return "\n".join(ascii_art11)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     text = CopyPasta().get() # gets a random copypasta
     if not text:
-        text = 'Failed to get copypasta :('
+        text = "Failed to get copypasta :("
 
     banner = makeblock(choice(ascii_art))
     ascii_art1 = []
